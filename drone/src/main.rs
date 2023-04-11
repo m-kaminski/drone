24d6169a60e39743f9ba4619bdf719cc638ff48f
Matt (Maciej) Kaminski <maciej.a.kaminski@gmail.com>
2023-04-11 14:26:31 +0200

    Now the game is displaying a bayraktar TB2 type drone

--- drone/src/main.rs
+++ drone/src/main.rs
@@ -58,6 +58,7 @@ fn gen_texture(ctx: &mut Context, w: usize, h: usize, tex: &str) -> Texture {
             match c {
                 'R' =>{ pixels[i] = 0xff;pixels[i+1] = 0x00;pixels[i+2] = 0x00; i+=4},
                 'B' =>{ pixels[i] = 0xFF;pixels[i+1] = 0x00;pixels[i+2] = 0xFF; i+=4},
+                'G' =>{ pixels[i] = 0x88;pixels[i+1] = 0x88;pixels[i+2] = 0x88; i+=4},
                 '_' =>{ pixels[i+3] = 0x00; i+=4},
                 _=>println!("unhandled key"),
                 }
@@ -67,8 +68,8 @@ fn gen_texture(ctx: &mut Context, w: usize, h: usize, tex: &str) -> Texture {
             ctx,
             &pixels,
             TextureParams {
-                width: 4,
-                height: 4,
+                width: w as u32,
+                height: h as u32,
                 format: TextureFormat::RGBA8,
                 wrap: TextureWrap::Clamp,
                 filter: FilterMode::Nearest, // use Linear for smooth
@@ -81,20 +82,49 @@ impl Drone {
        
         #[rustfmt::skip]
         let vertices: [Vertex; 4] = [
-            Vertex { pos : Vec2 { x: -0.5, y: -0.5 }, uv: Vec2 { x: 0., y: 0. } },
-            Vertex { pos : Vec2 { x:  0.5, y: -0.5 }, uv: Vec2 { x: 1., y: 0. } },
-            Vertex { pos : Vec2 { x:  0.5, y:  0.5 }, uv: Vec2 { x: 1., y: 1. } },
-            Vertex { pos : Vec2 { x: -0.5, y:  0.5 }, uv: Vec2 { x: 0., y: 1. } },
+            Vertex { pos : Vec2 { x: -0.2, y: 0.3 }, uv: Vec2 { x: 0., y: 0. } },
+            Vertex { pos : Vec2 { x:  0.2, y: 0.3 }, uv: Vec2 { x: 1., y: 0. } },
+            Vertex { pos : Vec2 { x:  0.2, y: -0.3 }, uv: Vec2 { x: 1., y: 1. } },
+            Vertex { pos : Vec2 { x: -0.2, y: -0.3 }, uv: Vec2 { x: 0., y: 1. } },
         ];
         let vertex_buffer = Buffer::immutable(ctx, BufferType::VertexBuffer, &vertices);
 
         let indices: [u16; 6] = [0, 1, 2, 0, 2, 3];
         let index_buffer = Buffer::immutable(ctx, BufferType::IndexBuffer, &indices);
-        let texture = gen_texture(ctx, 4,4,
-r#"_BB_
-BRRB
-BRRB
-_BB_"#);
+        let texture = gen_texture(ctx, 64,64,
+r#"________________________________________________________________
+________________________________________________________________
+________________________________________________________________
+________________________________________________________________
+________________________________________________________________
+________________________________________________________________
+________________________________________________________________
+__GGGGGG________________________________________________________
+__GGGGGG________________________________________________________
+_GGGGGGG________________________________________________________
+_GGGGGGG_____________G__________________________________________
+_GGGGGGG_____________G__________________________________________
+_GGGGGGG_____________G__________________________________________
+GGGGGGGG_____________G__________________________________________
+GGGGGGGG_____________GGGGGGGGG__________________________________
+GGGGGGGG____________GGGGGGGGGGGGGGGGGGGGGGG_____________________
+GGGGGGGGGGGG_______GGGGGGGGGGGGGGGGGGGGGGGGGGGGG________________
+GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG____________
+___________GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG________
+_____________________G_GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG_______
+_____________________G__GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG
+_____________________G__GGG__GGGGGGGGGGGGGGGGGGGGGGGGGGGG_______
+_____________________G_________GG________GGGGGGGGGGGGG__________
+_______________________________GG________GGGGG_GG_______________
+_______________________________GG_________GGG__GG_______________
+______________________________GGGG______________GG______________
+______________________________GGGG_____________GGGG_____________
+_______________________________GG______________GGGG_____________
+________________________________________________GG______________
+________________________________________________________________
+________________________________________________________________
+________________________________________________________________
+"#);
         let bindings = Bindings {
             vertex_buffers: vec![vertex_buffer],
             index_buffer: index_buffer,
